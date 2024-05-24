d7bf67122feffcd47ef0ee74a0fe16534ad9551c
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-05-24 14:00:24 -0300

    makefile

--- Makefile.bat
+++ Makefile.bat
@@ -1,8 +1,11 @@
 @ECHO off
-SET CFLAGS = -std=c++17 -O2
-SET LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
-@REM g++ -std=c++17 -O2 src/main.cpp -o ./src/BuildGame -lglfw -I "D:\prograns\GLFW\glfw-3.4\include" -lvulkan -I "D:\prograns\VulkanSDK\1.3.283.0\Include" -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
+@REM SET CFLAGS = -std=c++17 -O2
+@REM SET LDFLAGS = -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
+@REM -I./external_library/glfw-3.4.bin.WIN64/include ./external_library/glfw-3.4.bin.WIN64/lib-mingw-w64/* 
+./external_library/VulkanSKD-1.3.283.0/Lib/cmake/*
+@REM -I./external_library/glm-1.0.1-light/glm
+@REM -I./external_library/VulkanSKD-1.3.283.0/Include ./external_library/VulkanSKD-1.3.283.0/Lib/*
 echo building...
-g++ -std=c++17 -O2 src/main.cpp -o ./src/BuildGamegcc -I C:/msys64/mingw64/include/ C:/msys64/mingw64/lib/libglfw3.dll.a -I C:/msys64/mingw64/include/vulkan/ -lpthread
+g++ -std=c++17 -O2 src/main.cpp -o ./build/BuildGame -I./ -I./external_library/VulkanSKD-1.3.283.0/Include ./external_library/VulkanSKD-1.3.283.0/Lib/* -I./external_library/glm-1.0.1-light/glm -I./external_library/glfw-3.4.bin.WIN64/include ./external_library/glfw-3.4.bin.WIN64/lib-mingw-w64/*
+.\build\BuildGame.exe
 @REM del "./src/BuildGame.exe"
-@REM -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr
