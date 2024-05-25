de05accaafc62ab8337130325061ef19da54ca0e
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-05-25 11:20:24 -0300

    compilo no windows fdp

--- Makefile.bat
+++ Makefile.bat
@@ -1,5 +1,5 @@
 @ECHO off
-@REM -I./external_library/glfw-3.4.bin.WIN64/include ./external_library/glfw-3.4.bin.WIN64/lib-mingw-w64/* 
+@REM -I./external_library/glfw-3.4.bin.WIN64/include ./external_library/glfw-3.4.bin.WIN64/lib-mingw-w64/libglfw3dll.a  ./external_library/glfw-3.4.bin.WIN64/lib-mingw-w64/glfw3.dll ./external_library/glfw-3.4.bin.WIN64/lib-static-ucrt/*
 @REM -I./external_library/glm-1.0.1-light/glm
 @REM -I./external_library/VulkanSKD-1.3.283.0/Include ./external_library/VulkanSKD-1.3.283.0/Lib/*.lib
 echo building...
