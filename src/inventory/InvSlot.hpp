8e62329b2b91ef69d8f732218076c5db5bbb9e94
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-06-01 11:17:10 -0300

    ...

---
+++ src/inventory/InvSlot.hpp
@@ -0,0 +1,16 @@
+#ifndef INVSLOT_HPP
+#define INVSLOT_HPP
+
+#include "./../item/AbstractItem.hpp"
+
+class InvSlot
+{
+private:
+    /* data */
+public:
+    InvSlot(/* args */);
+    ~InvSlot();
+};
+
+
+#endif
